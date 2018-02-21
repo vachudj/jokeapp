28a9d653cb4e02908ab9828d0f357dd3ecf2e64f
jan.vachuda <jan.vachuda@4finance.com>
2018-02-21 07:21:25 +0100

    4:54 Creating view layer.

--- src/main/java/training/springframework/joke/JokeappApplication.java
+++ src/main/java/training/springframework/joke/JokeappApplication.java
@@ -1,4 +1,4 @@
-package training.springframework.joke.jokeapp;
+package training.springframework.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
