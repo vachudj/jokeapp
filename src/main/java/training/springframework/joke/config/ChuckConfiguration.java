34e1bd86c2a10a649c65ebfec7655b84c77d4165
jan.vachuda <jan.vachuda@4finance.com>
2018-02-23 07:44:04 +0100

    4:56 Java configuration introduction.

---
+++ src/main/java/training/springframework/joke/config/ChuckConfiguration.java
@@ -0,0 +1,16 @@
+package training.springframework.joke.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Configuration
+public class ChuckConfiguration {
+    
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+
+}
--- src/main/java/training/springframework/joke/services/JokeServiceImpl.java
+++ src/main/java/training/springframework/joke/services/JokeServiceImpl.java
@@ -8,9 +8,9 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
-    
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
